efcc9650d71ce70ccce8e0a18a28481733cc3321
hman124 <hman124@users.noreply.github.com>
2020-07-08 18:43:54 -0500

    added stuff

--- js/homepage.js
+++ js/homepage.js
@@ -1,7 +1,13 @@
-$("p").click(function() {
-  alert("test!!");
+$("p").mouseenter(function() {
+  clearInterval(anim);
 });
-var speed = [(Math.floor(Math.random() * 3) + 1), (Math.floor(Math.random() * 3) + 1), (Math.floor(Math.random() * 3) + 1), (Math.floor(Math.random() * 3) + 1), (Math.floor(Math.random() * 3) + 1), (Math.floor(Math.random() * 3) + 1), (Math.floor(Math.random() * 3) + 1), (Math.floor(Math.random() * 3) + 1), (Math.floor(Math.random() * 3) + 1), (Math.floor(Math.random() * 3) + 1)];
+
+$("p").mouseleave(function() {
+ anim = setInterval(menuanim, 40);
+});
+
+//Left - Right
+var speed = [(Math.floor(Math.random() * 4) + 1), (Math.floor(Math.random() * 4) + 1), (Math.floor(Math.random() * 4) + 1), (Math.floor(Math.random() * 4) + 1), (Math.floor(Math.random() * 4) + 1), (Math.floor(Math.random() * 4) + 1), (Math.floor(Math.random() * 4) + 1), (Math.floor(Math.random() * 4) + 1), (Math.floor(Math.random() * 4) + 1), (Math.floor(Math.random() * 4) + 1)];
 var playstate = ["forwards", "forwards", "forwards", "forwards", "forwards", "forwards", "forwards", "forwards", "forwards", "forwards", "forwards"];
 
 function menuanim(){
@@ -10,43 +16,20 @@ var left = parseInt($($(".item").get(i)).css("padding-left"));
 if(playstate[i] == "forwards"){
   if(left < Math.round(window.innerWidth - (parseInt($($(".item p").get(i)).css("width")) + 10))){
   left += speed[i];
-  $($(".item").get(i)).css("padding-left",  left);
+  $($(".item").get(i)).css("padding-left",  left + "px");
 }else{
     playstate[i] = "backwards";}}
 else{
   if(left != 0){
   left -= speed[i];
-    $($(".item").get(i)).css("padding-left",  left);}
+    $($(".item").get(i)).css("padding-left",  left + "px");}
     else{
     playstate[i] = "forwards";
   }}}}
 
-setInterval(menuanim, 10);
+var anim = setInterval(menuanim, 40);
 
-/*
-const items = document.getElementsByClassName("item");
-var playstate = "forwards";
+//Up - Down
 
-function menuanim() {
-  for (var i = 0; i < (items.length + 1); i++) {
-    var lpadding = parseInt($($(".item").get(i)).css("padding-left"));
-    if (playstate == "forwards") {
-      if (lpadding < (window.innerWidth - parseInt($($(".item p").get(i)).css("width")))) {
-        var padding = lpadding;
-        padding++;
-        $($(".item").get(i)).css("padding-left", padding);
-      } else {
-        playstate = "backwards";
-      }
-    } else {
-      if (padding >= (window.innerWidth - parseInt($($(".item p").get(i)).css("width")))) {
-        var padding = lpadding;
-        padding--;
-        $($(".item").get(i)).css("padding-left", padding);
-      } else {
-        playstate = "forwards";
-      }
-    }
-  }
-}
-*/
+//Rotating
+
